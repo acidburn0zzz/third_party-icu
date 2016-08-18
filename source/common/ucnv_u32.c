93efa67adf0ea4dde4bee55189228f5c287de445
Michael Jurka <mikejurka@google.com>
2016-08-17 23:40:57 -0700

    Upgrade to ICU 57.1, step two
    Fixed and applied patches
    Removed obsolete patches
    Minor source changes to fix compiler errors
    Update README.fuchsia to reflect changes to patches
    
    Applied:
    patches/locale_google.patch (fixed some issues during apply)
    patches/locale1.patch
    patches/linebrk.patch
    patches/wordbrk.patch (updated)
    patches/khmer-dictbe.patch
    patches/cjdict.patch (updated)
    patches/wpo.patch
    patches/data.build.patch
    patches/data_symb.patch
    patches/tzdetect.patch
    patches/utf32.patch
    patches/remove_si.patch
    patches/locale_extra.patch (updated)
    
    Removed obsolete patch files
    (obsolete after upgrade to 57-1)
    patches/emoji_props.patch
    patches/measure_format.patch
    patches/regexcmp.patch
    patches/relative_date.patch
    patches/utext.patch
    patches/xlit_badcast.patch
    
    Updated time zone information
    ./scripts/update_tz.sh 2016f
    
    Change-Id: I4360d8dd9e132a3bb9f4a08c378cfd8386155f2a

--- source/common/ucnv_u32.c
+++ source/common/ucnv_u32.c
@@ -16,7 +16,7 @@
 
 #include "unicode/utypes.h"
 
-#if !UCONFIG_NO_CONVERSION && !UCONFIG_ONLY_HTML_CONVERSION
+#if !UCONFIG_NO_CONVERSION
 
 #include "unicode/ucnv.h"
 #include "unicode/utf.h"
@@ -480,7 +480,9 @@ static const UConverterImpl _UTF32BEImpl = {
     NULL,
     NULL,
     NULL,
-    ucnv_getNonSurrogateUnicodeSet
+    ucnv_getNonSurrogateUnicodeSet,
+    NULL,
+    NULL,
 };
 
 /* The 1232 CCSID refers to any version of Unicode with any endianess of UTF-32 */
@@ -972,7 +974,9 @@ static const UConverterImpl _UTF32LEImpl = {
     NULL,
     NULL,
     NULL,
-    ucnv_getNonSurrogateUnicodeSet
+    ucnv_getNonSurrogateUnicodeSet,
+    NULL,
+    NULL,
 };
 
 /* The 1232 CCSID refers to any version of Unicode with any endianess of UTF-32 */
@@ -1214,7 +1218,9 @@ static const UConverterImpl _UTF32Impl = {
     NULL,
     NULL,
     NULL,
-    ucnv_getNonSurrogateUnicodeSet
+    ucnv_getNonSurrogateUnicodeSet,
+    NULL,
+    NULL
 };
 
 /* The 1236 CCSID refers to any version of Unicode with a BOM sensitive endianess of UTF-32 */
--- source/common/ucnv_u8.c
+++ source/common/ucnv_u8.c
@@ -1072,7 +1072,9 @@ static const UConverterImpl _CESU8Impl={
     NULL,
     NULL,
     NULL,
-    ucnv_getCompleteUnicodeSet
+    ucnv_getCompleteUnicodeSet,
+    NULL,
+    NULL
 };
 
 static const UConverterStaticData _CESU8StaticData={
